1d1859f1b98dd1d6638de1b5987f0a5324d95acc
Maciej Rożnawski <intro202@wp.pl>
2024-02-07 09:56:58 +0000

    add some styles and controller

---
+++ Pierwszyprojekt/resources/js/Pages/Index/Index.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const Index = () => {
+    return (
+        <body>
+            <nav class="flex flex-row justify-between items-center w-full h-20  gap-4 pr-[120px] pl-[120px]">
+                <div class="flex w-[200px] h-10 bg-purple-500  items-center justify-center">
+                    Logo
+                </div>
+                <div class="flex w-auto h-10  ">
+                    <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 ">
+                        <div class="hover:text-gray-500 underline-animation underline-offset-8	">
+                            Lorem Ipsum
+                        </div>
+                    </div>
+                    <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 ">
+                        <div class="hover:text-gray-500 underline-animation underline-offset-8	">
+                            Lorem Ipsum
+                        </div>
+                    </div>
+                    <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 ">
+                        <div class="hover:text-gray-500 underline-animation underline-offset-8	">
+                            Lorem Ipsum
+                        </div>
+                    </div>
+                    <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 ">
+                        <div class="hover:text-gray-500 underline-animation underline-offset-8	">
+                            Lorem Ipsum
+                        </div>
+                    </div>
+                    <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 ">
+                        <div class="hover:text-gray-500 underline-animation underline-offset-8	">
+                            Lorem Ipsum
+                        </div>
+                    </div>
+                    <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 ">
+                        <div class="hover:text-gray-500 underline-animation underline-offset-8	">
+                            Lorem Ipsum
+                        </div>
+                    </div>
+                    <div class="w-auto h-10 flex items-center justify-center bg-yellow-400 rounded-[25px] p-4">
+                        <div class="hover:text-gray-500 underline-animation underline-offset-8	">
+                            Lorem Ipsum
+                        </div>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="w-[200px] h-10 border-[2px] boder-black rounded-3xl"></div>
+                </div>
+            </nav>
+            <div class="flex flex-row bg-red-500 w-full h-[80px] justify-center items-center">
+                Picture
+            </div>
+            <div class="flex justify-center items-center w-full custom-height">
+                <div class="grid grid-cols-8 w-[1200px]  h-2/3 ">
+                    <div class="bg-black  col-start-1 col-end-4 mr-[20px]"></div>
+                    <div class="bg-orange-500  col-start-4 col-end-8"></div>
+                </div>
+            </div>
+            <footer></footer>
+        </body>
+    );
+};
+
+export default Index;
--- Pierwszyprojekt/resources/views/app.blade.php
+++ Pierwszyprojekt/resources/views/app.blade.php
@@ -3,20 +3,18 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @routes
-        @viteReactRefresh
-        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+        <title>Laravel</title>
+        @viteReactRefresh 
+        @vite('resources/js/app.jsx')
         @inertiaHead
+        @routes
     </head>
-    <body class="font-sans antialiased">
-        @inertia
+
+    <body class="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300 font-serif">
+      @inertia
+
     </body>
 </html>
