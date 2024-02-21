85c1b351ed6bc4a822a543e3ed0084cd618311c1
Michal Tajchert <michaltajchert@gmail.com>
2024-02-21 13:08:34 +0100

    Update

--- resources/js/Pages/Index/Index.jsx
+++ resources/js/Pages/Index/Index.jsx
@@ -36,54 +36,27 @@ const Index = ({ countries }) => {
 
   return (
     <body>
-      <nav class="flex flex-row justify-between items-center w-full h-20  gap-4 pr-[120px] pl-[120px]">
-        <div class="flex w-[200px] h-10 bg-purple-500  items-center justify-center">
-          Logo
+      <nav class="flex flex-row justify-between items-center w-full my-3 gap-4 pr-[120px] pl-[120px]">
+        <div class="flex w-[200px] bg-purple-500  items-center justify-center">
+          <img alt={'logo'} src={'/images/logo.jpg'} />
         </div>
-        <div class="flex w-auto h-10  ">
-          <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 ">
-            <div class="hover:text-gray-500 underline-animation underline-offset-8	">
-              Lorem Ipsum
-            </div>
-          </div>
-          <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 ">
-            <div class="hover:text-gray-500 underline-animation underline-offset-8	">
-              Lorem Ipsum
-            </div>
-          </div>
-          <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 ">
-            <div class="hover:text-gray-500 underline-animation underline-offset-8	">
-              Lorem Ipsum
-            </div>
-          </div>
-          <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 ">
-            <div class="hover:text-gray-500 underline-animation underline-offset-8	">
-              Lorem Ipsum
-            </div>
-          </div>
-          <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 ">
-            <div class="hover:text-gray-500 underline-animation underline-offset-8	">
-              Lorem Ipsum
-            </div>
-          </div>
-          <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 ">
-            <div class="hover:text-gray-500 underline-animation underline-offset-8	">
-              Lorem Ipsum
-            </div>
-          </div>
-          <div class="w-auto h-10 flex items-center justify-center bg-yellow-400 rounded-[25px] p-4">
-            <div class="hover:text-gray-500 underline-animation underline-offset-8	">
-              Lorem Ipsum
-            </div>
+        <div class="flex w-auto">
+          <div class="w-auto h-10 flex items-center justify-center ml-4 mr-4 gap-10">
+            <a href="#channel-list" className="!text-white underline-animation underline-offset-8	">
+              Channel List
+            </a>
+            <a href="#faq" className="!text-white underline-animation underline-offset-8	">
+              FAQ
+            </a>
+            <a href="#contact-us" className="!text-white underline-animation underline-offset-8	">
+              Contact Us
+            </a>
           </div>
         </div>
       </nav>
-      <div class="flex flex-row bg-red-500 w-full h-[100px] justify-center items-center">
-        Picture
-      </div>
-      <div class="flex justify-center  w-full h-full">
+      <div id="channel-list" class="flex justify-center w-full h-full">
         <div class=" grid grid-cols-1 w-2/3  h-2/3 ">
-          <div class="bg-black p-2 mt-10 text-white">
+          <div class="bg-black bg-opacity-75 p-2 mt-10 text-white">
             <div>
               <div className="flex w-full h-[800px] ">
                 <ul className="flex flex-col w-1/3 overflow-auto">
@@ -138,7 +111,7 @@ const Index = ({ countries }) => {
           </div>
         </div>
       </div>
-      <div class="flex justify-center items-center w-full h-auto mt-[20px] p-10 ">
+      <div class="flex mx-auto max-w-screen-md justify-center items-center w-full h-auto py-16 ">
         <div className="flex flex-col w-full  ">
           {aboutUs.map(({ id, name, content }) => (
             <div key={id} className="my-2 w-full">
@@ -148,7 +121,7 @@ const Index = ({ countries }) => {
               >
                 {openDivs.includes(id) ? (
                   <ion-icon class="mr-2" name="arrow-up-circle-outline">
-                    {" "}
+                    {"Answer"}
                   </ion-icon>
                 ) : (
                   <ion-icon
@@ -168,47 +141,43 @@ const Index = ({ countries }) => {
           ))}
         </div>
       </div>
-
-      <div class=" w-full h-[500px] mt-[20px]  flex justify-center items-center">
-        <div class="w-2/3 flex justify-center">
-          <div class=" h-[500px] w-1/2  ">
-            <div class="flex flex-col mr-10 ml-10 items-center p-10">
-              <h2>Contat us</h2>
-              <a class="mb-4">
-                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-                Lorem Ipsum Lorem Ipsum Lorem Ipsum
-              </a>
-              <input
-                class="input-normal  w-full"
-                type="text"
-                placeholder="Lorem Ipsum"
-              ></input>
-              <input
-                class="input-normal w-full"
-                type="text"
-                placeholder="Lorem Ipsum"
-              ></input>
-              <input
-                class="input-normal w-full"
-                type="text"
-                placeholder="Lorem Ipsum"
-              ></input>
-              <textarea
-                id="w3review"
-                name="w3review"
-                rows="4"
-                cols="50"
-                class="input-normal w-full "
-                placeholder="Lorem Ipsum"
-              ></textarea>
-              <button class="uppercase w-full h-[40px] mt-[15px] bg-black text-white">
-                send
-              </button>
+      
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact
+            Us</h2>
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a
+            technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us
+            know.</p>
+          <form action="#" className="space-y-8">
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your
+                email</label>
+              <input type="email" id="email"
+                     className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                     placeholder="name@flowbite.com" required/>
             </div>
-          </div>
+            <div>
+              <label htmlFor="subject"
+                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+              <input type="text" id="subject"
+                     className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                     placeholder="Let us know how we can help you" required/>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
+                message</label>
+              <textarea id="message" rows="6"
+                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        placeholder="Leave a comment..."></textarea>
+            </div>
+            <button type="submit"
+                    className="py-3 px-5 text-sm font-medium text-center rounded-lg bg-white sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send
+              message
+            </button>
+          </form>
         </div>
-      </div>
-      <footer class="bg-black w-full h-[220px] mt-[100px]"></footer>
+      </section>
     </body>
   );
 };
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,11 +5,11 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Shaq.TV';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx'), ),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
